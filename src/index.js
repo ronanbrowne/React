d06c96a2d6cbd53e531bbf1fb369d0553f9436e4
Ronan Browne <ronanbrowne88@gmail.com>
2019-01-20 21:35:22 +0000

    more JSX syntax
    working with styling basics

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import ReactDOM from 'react-dom'
 
 // Create a react component
 const App = () => {
-    return <div>Hi There</div>;
+    return (
+    <div>
+        <label className="lable" htmlFor="name">Enter Name: </label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+    </div>
+);
 }
 
 
@@ -14,5 +20,4 @@ const App = () => {
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
-
 );
